9fdf99701e5b5c8218454848db5af32bce6943cb
Sean Adkinson <sean.adkinson@streem.pro>
2018-06-04 20:39:44 -0700

    Selecting all phones and emails from the contact... prompt for phone selection

--- android/app/src/main/java/com/lynxit/contactswrapper/ContactsWrapper.java
+++ android/app/src/main/java/com/lynxit/contactswrapper/ContactsWrapper.java
@@ -2,63 +2,34 @@ package com.lynxit.contactswrapper;
 
 import android.app.Activity;
 import android.content.ContentResolver;
-import android.content.Context;
 import android.content.Intent;
 import android.database.Cursor;
 import android.net.Uri;
-import android.provider.ContactsContract;
-import android.util.Log;
+import android.provider.ContactsContract.CommonDataKinds.Email;
+import android.provider.ContactsContract.CommonDataKinds.Phone;
+import android.provider.ContactsContract.CommonDataKinds.StructuredName;
+import android.provider.ContactsContract.Contacts;
+import android.provider.ContactsContract.Contacts.Entity;
 
-import java.net.URI;
-import java.util.*;
-
-import com.facebook.react.*;
-
-import com.facebook.react.ReactPackage;
 import com.facebook.react.bridge.ActivityEventListener;
 import com.facebook.react.bridge.Arguments;
-import com.facebook.react.bridge.NativeModule;
-import com.facebook.react.bridge.JavaScriptModule;
 import com.facebook.react.bridge.Promise;
 import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.bridge.ReactContextBaseJavaModule;
 import com.facebook.react.bridge.ReactMethod;
-import com.facebook.react.bridge.ReadableArray;
-import com.facebook.react.bridge.ReadableMap;
-import com.facebook.react.bridge.ReadableMapKeySetIterator;
-import com.facebook.react.bridge.ReadableType;
+import com.facebook.react.bridge.WritableArray;
 import com.facebook.react.bridge.WritableMap;
-import com.facebook.react.uimanager.ViewManager;
 
 public class ContactsWrapper extends ReactContextBaseJavaModule implements ActivityEventListener {
 
-    private static final int CONTACT_REQUEST = 1111;
-    private static final int EMAIL_REQUEST = 2222;
+    private static final int CONTACT_REQUEST = 11112;
     public static final String E_CONTACT_CANCELLED = "E_CONTACT_CANCELLED";
     public static final String E_CONTACT_NO_DATA = "E_CONTACT_NO_DATA";
-    public static final String E_CONTACT_NO_EMAIL = "E_CONTACT_NO_EMAIL";
     public static final String E_CONTACT_EXCEPTION = "E_CONTACT_EXCEPTION";
     public static final String E_CONTACT_PERMISSION = "E_CONTACT_PERMISSION";
     private Promise mContactsPromise;
     private Activity mCtx;
     private final ContentResolver contentResolver;
-    private static final List<String> JUST_ME_PROJECTION = new ArrayList<String>() {{
-        add(ContactsContract.Contacts.Data.MIMETYPE);
-        add(ContactsContract.Profile.DISPLAY_NAME);
-        add(ContactsContract.CommonDataKinds.Contactables.PHOTO_URI);
-        add(ContactsContract.CommonDataKinds.StructuredName.DISPLAY_NAME);
-        add(ContactsContract.CommonDataKinds.StructuredName.GIVEN_NAME);
-        add(ContactsContract.CommonDataKinds.StructuredName.MIDDLE_NAME);
-        add(ContactsContract.CommonDataKinds.StructuredName.FAMILY_NAME);
-        add(ContactsContract.CommonDataKinds.Phone.NUMBER);
-        add(ContactsContract.CommonDataKinds.Phone.TYPE);
-        add(ContactsContract.CommonDataKinds.Phone.LABEL);
-        add(ContactsContract.CommonDataKinds.Email.DATA);
-        add(ContactsContract.CommonDataKinds.Email.ADDRESS);
-        add(ContactsContract.CommonDataKinds.Email.TYPE);
-        add(ContactsContract.CommonDataKinds.Email.LABEL);
-    }};
-
 
     public ContactsWrapper(ReactApplicationContext reactContext) {
         super(reactContext);
@@ -68,164 +39,177 @@ public class ContactsWrapper extends ReactContextBaseJavaModule implements Activ
 
     @Override
     public String getName() {
-        return "ContactsWrapper";
+        return "SelectContact";
     }
 
 
-
     @ReactMethod
-    public void getContact(Promise contactsPromise) {
+    public void openContactSelection(Promise contactsPromise) {
         launchPicker(contactsPromise, CONTACT_REQUEST);
-    }
-
-    @ReactMethod
-    public void getEmail(Promise contactsPromise) {
-        launchPicker(contactsPromise, EMAIL_REQUEST);
     }
 
     /**
      * Lanch the contact picker, with the specified requestCode for returned data.
+     *
      * @param contactsPromise - promise passed in from React Native.
-     * @param requestCode - request code to specify what contact data to return
+     * @param requestCode     - request code to specify what contact data to return
      */
     private void launchPicker(Promise contactsPromise, int requestCode) {
-//        this.contentResolver.query(Uri.parse("content://com.android.contacts/contacts/lookup/0r3-A7416BA07AEA92F2/3"), null, null, null, null);
-        Cursor cursor = this.contentResolver.query(ContactsContract.Contacts.CONTENT_URI, null, null, null, null);
+        Cursor cursor = this.contentResolver.query(Contacts.CONTENT_URI, null, null, null, null);
         if (cursor != null) {
             mContactsPromise = contactsPromise;
             Intent intent = new Intent(Intent.ACTION_PICK);
-            intent.setType(ContactsContract.Contacts.CONTENT_TYPE);
+            intent.setType(Contacts.CONTENT_TYPE);
             mCtx = getCurrentActivity();
             if (intent.resolveActivity(mCtx.getPackageManager()) != null) {
                 mCtx.startActivityForResult(intent, requestCode);
             }
             cursor.close();
-        }else{
+        } else {
             mContactsPromise.reject(E_CONTACT_PERMISSION, "no permission");
         }
     }
 
     @Override
-    public void onActivityResult(Activity ContactsWrapper, final int requestCode, final int resultCode, final Intent intent) {
-
-        if(mContactsPromise == null || mCtx == null
-              || (requestCode != CONTACT_REQUEST && requestCode != EMAIL_REQUEST)){
-          return;
+    public void onActivityResult(final int requestCode, final int resultCode, Intent intent) {
+        if (mContactsPromise == null || mCtx == null || requestCode != CONTACT_REQUEST) {
+            return;
         }
 
-        String email = null;
-        switch (resultCode) {
-            case (Activity.RESULT_OK):
-                Uri contactUri = intent.getData();
-                switch(requestCode) {
-                    case(CONTACT_REQUEST):
-                        try {
-                            /* Retrieve all possible data about contact and return as a JS object */
+        //Request was cancelled
+        if (resultCode != Activity.RESULT_OK) {
+            mContactsPromise.reject(E_CONTACT_CANCELLED, "Cancelled");
+            return;
+        }
 
-                            //First get ID
-                            String id = null;
-                            int idx;
-                            WritableMap contactData = Arguments.createMap();
-                            Cursor cursor = this.contentResolver.query(contactUri, null, null, null, null);
-                            if (cursor != null && cursor.moveToFirst()) {
-                                idx = cursor.getColumnIndex(ContactsContract.Contacts._ID);
-                                id = cursor.getString(idx);
-                            } else {
-                                mContactsPromise.reject(E_CONTACT_NO_DATA, "Contact Data Not Found");
-                                return;
-                            }
+        // Retrieve all possible data about contact and return as a JS object
+        WritableMap contactData = Arguments.createMap();
 
+        try {
+            String id = getContactId(intent.getData());
+            Uri contactUri = buildContactUri(id);
+            boolean foundData = false;
 
-                            // Build the Entity URI.
-                            Uri.Builder b = Uri.withAppendedPath(ContactsContract.Contacts.CONTENT_URI, id).buildUpon();
-                            b.appendPath(ContactsContract.Contacts.Entity.CONTENT_DIRECTORY);
-                            contactUri = b.build();
+            Cursor cursor = openContactQuery(contactUri);
+            if (cursor.moveToFirst()) {
+                do {
+                    String mime = cursor.getString(cursor.getColumnIndex(Entity.MIMETYPE));
+                    switch (mime) {
+                        case StructuredName.CONTENT_ITEM_TYPE:
+                            addNameData(contactData, cursor);
+                            foundData = true;
+                            break;
 
-                            // Create the projection (SQL fields) and sort order.
-                            String[] projection = {
-                                ContactsContract.Contacts.Entity.MIMETYPE,
-                                ContactsContract.Contacts.Entity.DATA1
-                            };
-                            String sortOrder = ContactsContract.Contacts.Entity.RAW_CONTACT_ID + " ASC";
-                            cursor = this.contentResolver.query(contactUri, projection, null, null, sortOrder);
-                            if(cursor == null)  return;
+                        case Phone.CONTENT_ITEM_TYPE:
+                            addPhoneData(contactData, cursor, mCtx);
+                            foundData = true;
+                            break;
 
-                            String mime;
-                            boolean foundData = false;
-                            /* Map Any contact data we want returned to the JS object key for React Native */
-                            HashMap<String, String> returnKeys = new HashMap<String, String>();
-                            returnKeys.put(ContactsContract.CommonDataKinds.StructuredName.CONTENT_ITEM_TYPE, "name");
-                            returnKeys.put(ContactsContract.CommonDataKinds.Phone.CONTENT_ITEM_TYPE, "phone");
-                            returnKeys.put(ContactsContract.CommonDataKinds.Email.CONTENT_ITEM_TYPE, "email");
+                        case Email.CONTENT_ITEM_TYPE:
+                            addEmailData(contactData, cursor, mCtx);
+                            foundData = true;
+                            break;
+                    }
+                } while (cursor.moveToNext());
+            }
+            cursor.close();
 
-                            int dataIdx = cursor.getColumnIndex(ContactsContract.Contacts.Entity.DATA1);
-                            int mimeIdx = cursor.getColumnIndex(ContactsContract.Contacts.Entity.MIMETYPE);
-                            if (cursor.moveToFirst()) {
-                                do {
-                                    mime = cursor.getString(mimeIdx);
-                                    if(returnKeys.containsKey(mime)) {
-                                        contactData.putString((String) returnKeys.get(mime), cursor.getString(dataIdx));
-                                        foundData = true;
-                                    }
-                                } while (cursor.moveToNext());
-                            }
-
-                            cursor.close();
-                            if(foundData) {
-                                mContactsPromise.resolve(contactData);
-                                return;
-                            } else {
-                                mContactsPromise.reject(E_CONTACT_NO_DATA, "No data found for contact");
-                                return;
-                            }
-                        } catch (Exception e) {
-                            mContactsPromise.reject(E_CONTACT_EXCEPTION, e.getMessage());
-                            return;
-                        }
-                        /* No need to break as all paths return */
-                    case(EMAIL_REQUEST):
-                        /* Return contacts first email address, as string */
-                        try {
-
-
-                            // get the contact id from the Uri
-                            String id = contactUri.getLastPathSegment();
-
-                            // query for everything email
-                            Cursor cursor = mCtx.getContentResolver().query(ContactsContract.CommonDataKinds.Email.CONTENT_URI,
-                                                                            null, ContactsContract.CommonDataKinds.Email.CONTACT_ID + "=?", new String[]{id},
-                                                                            null);
-
-                            int emailIdx = cursor.getColumnIndex(ContactsContract.CommonDataKinds.Email.DATA);
-
-                            // For now, return only the first email address, as a string
-                            if (cursor.moveToFirst()) {
-                                email = cursor.getString(emailIdx);
-                                mContactsPromise.resolve(email);
-                                return;
-                            } else {
-                                //Contact has no email address stored
-                                mContactsPromise.reject(E_CONTACT_NO_EMAIL, "No email found for contact");
-                                return;
-                            }
-                        } catch (Exception e) {
-                            mContactsPromise.reject(E_CONTACT_EXCEPTION, e.getMessage());
-                            return;
-                        }
-                        /* No need to break as all paths return */
-                    default:
-                        //Unexpected return code - shouldn't happen, but catch just in case
-                        mContactsPromise.reject(E_CONTACT_EXCEPTION, "Unexpected error in request");
-                        return;
-                }
-            default:
-                //Request was cancelled
-                mContactsPromise.reject(E_CONTACT_CANCELLED, "Cancelled");
-                return;
+            if (foundData) {
+                mContactsPromise.resolve(contactData);
+            } else {
+                mContactsPromise.reject(E_CONTACT_NO_DATA, "No data found for contact");
+            }
+        } catch (Exception e) {
+            mContactsPromise.reject(E_CONTACT_EXCEPTION, e.getMessage());
         }
     }
 
-    public void onNewIntent(Intent intent) {
+    private String getContactId(Uri contactUri) throws SelectContactException {
+        Cursor cursor = this.contentResolver.query(contactUri, null, null, null, null);
+        if (cursor == null || !cursor.moveToFirst()) {
+            throw new SelectContactException(E_CONTACT_NO_DATA, "Contact Data Not Found");
+        }
 
+        return cursor.getString(cursor.getColumnIndex(Contacts._ID));
+    }
+
+    private Uri buildContactUri(String id) {
+        return Uri
+                .withAppendedPath(Contacts.CONTENT_URI, id)
+                .buildUpon()
+                .appendPath(Entity.CONTENT_DIRECTORY)
+                .build();
+    }
+
+    private Cursor openContactQuery(Uri contactUri) throws SelectContactException {
+        String[] projection = {
+                Entity.MIMETYPE,
+                Entity.DATA1,
+                Entity.DATA2,
+                Entity.DATA3
+        };
+        String sortOrder = Entity.RAW_CONTACT_ID + " ASC";
+        Cursor cursor = this.contentResolver.query(contactUri, projection, null, null, sortOrder);
+        if (cursor == null) {
+            throw new SelectContactException(E_CONTACT_EXCEPTION, "Could not query contacts data. Unable to create cursor.");
+        }
+
+        return cursor;
+    }
+
+    private void addNameData(WritableMap contactData, Cursor cursor) {
+        int displayNameIndex = cursor.getColumnIndex(StructuredName.DISPLAY_NAME);
+        contactData.putString("name", cursor.getString(displayNameIndex));
+    }
+
+    private void addPhoneData(WritableMap contactData, Cursor cursor, Activity activity) {
+        WritableArray phoneNumbers = (WritableArray) contactData.getArray("phones");
+        if (phoneNumbers == null) {
+            phoneNumbers = Arguments.createArray();
+            contactData.putArray("phones", phoneNumbers);
+        }
+
+        String phoneNumber = cursor.getString(cursor.getColumnIndex(Phone.NUMBER));
+        int phoneType = cursor.getInt(cursor.getColumnIndex(Phone.TYPE));
+        String phoneLabel = cursor.getString(cursor.getColumnIndex(Phone.LABEL));
+        CharSequence typeLabel = Phone.getTypeLabel(activity.getResources(), phoneType, phoneLabel);
+
+        WritableMap phoneEntry = Arguments.createMap();
+        phoneEntry.putString("number", phoneNumber);
+        phoneEntry.putString("type", String.valueOf(typeLabel));
+
+        phoneNumbers.pushMap(phoneEntry);
+    }
+
+    private void addEmailData(WritableMap contactData, Cursor cursor, Activity activity) {
+        WritableArray emails = (WritableArray) contactData.getArray("emails");
+        if (emails == null) {
+            emails = Arguments.createArray();
+            contactData.putArray("emails", emails);
+        }
+
+        String emailAddress = cursor.getString(cursor.getColumnIndex(Email.ADDRESS));
+        int emailType = cursor.getInt(cursor.getColumnIndex(Email.TYPE));
+        String emailLabel = cursor.getString(cursor.getColumnIndex(Email.LABEL));
+        CharSequence typeLabel = Email.getTypeLabel(activity.getResources(), emailType, emailLabel);
+
+        WritableMap emailEntry = Arguments.createMap();
+        emailEntry.putString("address", emailAddress);
+        emailEntry.putString("type", String.valueOf(typeLabel));
+
+        emails.pushMap(emailEntry);
+    }
+
+    public static class SelectContactException extends Exception {
+        private final String errorCode;
+
+        public SelectContactException(String errorCode, String errorMessage) {
+            super(errorMessage);
+            this.errorCode = errorCode;
+        }
+
+        public String getErrorCode() {
+            return errorCode;
+        }
     }
 }
--- index.js
+++ index.js
@@ -1,2 +1,50 @@
-var ReactNative = require('react-native')
-module.exports = ReactNative.NativeModules.ContactsWrapper
+'use strict';
+
+import {
+    NativeModules,
+    NativeEventEmitter,
+    ActionSheet,
+    Platform
+} from 'react-native';
+import ActionSheet from '@yfuks/react-native-action-sheet';
+
+const { SelectContact } = NativeModules;
+
+
+module.exports = {
+
+    selectContactPhone() {
+        return SelectContact.openContactSelection()
+            .then(contact => selectPhone(contact));
+    }
+
+};
+
+function selectPhone(contact) {
+    let phones = contact && contact.phones || [];
+    if (phones.length < 2) {
+        return phones[0];
+    }
+
+    let options = phones.map(phone => {
+        let { number, type } = phone;
+        return number + (type ? ` - ${type}` : '');
+    });
+
+    if (Platform.OS === 'ios') {
+        options.push('Cancel');
+    }
+
+    return new Promise(((resolve) => {
+        ActionSheet.showActionSheetWithOptions({
+                options: options,
+                cancelButtonIndex: options.length - 1,
+                tintColor: 'blue'
+            },
+            (buttonIndex) => {
+                let selected = phones[buttonIndex];
+                let phone = selected && selected.number || null;
+                resolve({ contact, phone });
+            });
+    }));
+}
